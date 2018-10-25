74daabee243475a9420feccce115d5d7e65828be
KryptosA <36996429+KryptosA@users.noreply.github.com>
2018-10-25 01:59:03 -0500

    Initial Commit

---
+++ FPS/Assets/CursorLock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorLock : MonoBehaviour {
+
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            System.Console.WriteLine("Unlocked");
+        }
+    }
+}
---
+++ FPS/Assets/DoorAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour {
+
+    private Animator doorAnim = null;
+
+    void Start()
+    {
+        doorAnim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        doorAnim.SetBool("character_nearby", true);
+        Debug.Log("true");
+    }
+
+   void OnTriggerExit(Collider other)
+    {
+        doorAnim.SetBool("character_nearby", false);
+        Debug.Log("false");
+    }
+}
